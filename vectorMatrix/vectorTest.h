1179988abcac979a50fa9ca4f8714ba063e00596
Romain PERRIN <romain.perrin@etu.unistra.fr>
2017-10-13 17:05:37 +0200

    Ajout des définitions de fonctions de test de la classe Vector

---
+++ vectorMatrix/vectorTest.h
@@ -0,0 +1,19 @@
+#ifndef __VECTORTEST_H__
+#define __VECTORTEST_H__
+
+#include "vector.h"
+
+bool testSomme();
+bool testDifference();
+bool testSommeAffectation();
+bool testDifferenceAffectation();
+
+bool testProduitParScalaire();
+bool testDivisionParScalaire();
+bool testProduitAffectationParScalaire();
+bool testDivisionAffectationParScalaire();
+
+bool testProduitScalaire();
+bool testProduitVectoriel();
+
+#endif
