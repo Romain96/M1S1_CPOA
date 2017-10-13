606244d6734ff82daa031f4de8b953778f101018
Romain PERRIN <romain.perrin@etu.unistra.fr>
2017-10-13 16:40:47 +0200

    Ajout des descriptions de fonctions de test la classe Array

---
+++ vectorMatrix/arrayTest.h
@@ -0,0 +1,12 @@
+#ifndef __ARRAYTEST_H__
+#define __ARRAYTEST_H__
+
+#include "array.h"
+
+bool testRemplissage();
+bool testAffectation();
+bool testEgalite();
+bool testNonEgalite();
+bool testEchange();
+
+#endif
