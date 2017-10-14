19e7fc8d0753fc8b0ead10472b38952b475b30b2
Romain PERRIN <romain.perrin@etu.unistra.fr>
2017-10-14 17:44:05 +0200

    Ajout des définitions des fonctions de test de la classe Matrix33d

---
+++ vectorMatrix/matrix33dTest.h
@@ -0,0 +1,9 @@
+#ifndef __MATRIX33DTEST_H__
+#define __MATRIX33DTEST_H__
+
+bool testInverse();
+bool testTranslationStatique();
+bool testRotationStatique();
+bool testHomothetieStatique();
+
+#endif
