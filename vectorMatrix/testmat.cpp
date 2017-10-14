012db6b960fbec2e89e91663fdb143c16b65514b
Romain PERRIN <romain.perrin@etu.unistra.fr>
2017-10-14 17:45:08 +0200

    Ajout des tests de la classe Matrix33 (temporaire avant de tout inclure dans le fichier test.cpp

---
+++ vectorMatrix/testmat.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "array.h"
+#include "matrix33d.h"
+#include "matrix33dTest.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+        // tests sur la classe Matrix33d
+        bool tInverse = testInverse();
+        bool tTranslationStatique = testTranslationStatique();
+        bool tRotationStatique = testRotationStatique();
+        bool tHomothetieStatique = testHomothetieStatique();
+
+        cout << "=============================================" << endl;
+        cout << "Programme de test de la classe Matrix33d" << endl;
+        cout << "=============================================" << endl;
+        cout << "Matrix33d : inversion de matrice = " << tInverse << endl;
+        cout << "Matrix33d : translation statique = " << tTranslationStatique << endl;
+        cout << "Matrix33d : rotation statique = " << tRotationStatique << endl;
+        cout << "Matrix33d : homothétie statique = " << tHomothetieStatique << endl;
+        cout << "=============================================" << endl;
+        if (tInverse && tTranslationStatique && tRotationStatique && tRotationStatique && tHomothetieStatique)
+                cout << "RESULTAT MATRIX33D : OK" << endl;
+        else
+                cout << "RESULTAT MATRIX33D : PAS OK" << endl;
+}
