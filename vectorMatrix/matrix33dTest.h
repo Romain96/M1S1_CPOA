4cddc4ea5a4afd0ea778204da6457c574ba21c19
Romain PERRIN <romain.perrin@etu.unistra.fr>
2017-10-21 18:50:39 +0200

    Ajout des déclarations des fonctions de test des nouvelles fonctions de multiplication des vacteurs et des matrices

--- vectorMatrix/matrix33dTest.h
+++ vectorMatrix/matrix33dTest.h
@@ -5,5 +5,9 @@ bool testInverse();
 bool testTranslationStatique();
 bool testRotationStatique();
 bool testHomothetieStatique();
+bool testMultiplicationVec3fMatrix33d();
+bool testMultiplicationMatrix33dVec3f();
+bool testMultiplicationVec2fMatrix33d();
+bool testMultiplicationMatrix33dVec2f();
 
 #endif
