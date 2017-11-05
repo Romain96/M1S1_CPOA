9f5d101427382cb1cb2d4686ee081280700dd2ae
Romain PERRIN <romain.perrin@etu.unistra.fr>
2017-11-05 14:12:32 +0100

    écriture de la méthode sous-echantillonage

--- image/image2grey.cpp
+++ image/image2grey.cpp
@@ -15,9 +15,25 @@ Image2Grey::~Image2Grey()
 // sous-échantilonage (retourne la taille de l'image par 2)
 Image2Grey Image2Grey::sous_echantillonage()
 {
-	// comment diviser ? au centre ? au coin supérieur gauche ? paramètres ?
-	Image2Grey se(this->getWidth()/2, this->getHeight()/2);
-	return se;
+	// création de la nouvelle image (taille/2)
+	Image2Grey imageDiv2(this->getWidth()/2, this->getHeight()/2);
+
+	// parcours de l'ancienne image et moyenne sur les 4 cases telles que
+	// nouvelle[i][j] = moyenne de ancienne[i][j], ancienne[i][j+1], ancienne[i+1][j], ancienne[i+1][j+1]
+	for (int i = 0; i < this->getHeight() - 1; i+=2)
+	{
+		for (int j = 0; j < this->getWidth() - 1; j+=2)
+		{
+			// somme des 4 valeurs
+			int val = pixels_[j + i*this->getWidth()] + pixels_[(j+1) + i*this->getWidth()] +
+				pixels_[j + (i+1)*this->getWidth()] + pixels_[(j+1) + (i+1)*this->getWidth()];
+			// moyenne
+			val = val/4;
+			// remplissage de la nouvelle image
+			imageDiv2(i/2, j/2) = val;
+		}
+	}
+	return imageDiv2;
 }
 
 // lissage paramétré par les (2n+1)^2 pixels voisins
